d9b961b38bf3230c771ed7edee5c7cee147f9c1e
vitran96 <tranuytrieuvi@gmail.com>
2022-08-18 19:33:53 +0700

    remove init task

--- helper/setup.ps1
+++ helper/setup.ps1
@@ -9,13 +9,13 @@ else {
 }
 
 # Install choco
-# if ([bool](Get-Command -Name 'choco' -ErrorAction SilentlyContinue)) {
-#     Write-Verbose "Chocolatey is already installed, skip installation." -Verbose
-# }
-# else {
-#     Write-Verbose "Installing Chocolatey..." -Verbose
-#     Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-# }
+if ([bool](Get-Command -Name 'choco' -ErrorAction SilentlyContinue)) {
+    Write-Verbose "Chocolatey is already installed, skip installation." -Verbose
+}
+else {
+    Write-Verbose "Installing Chocolatey..." -Verbose
+    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
 
 # Install and Start OpenSSH Server
 if ([bool](Get-Service -Name sshd -ErrorAction SilentlyContinue)) {
